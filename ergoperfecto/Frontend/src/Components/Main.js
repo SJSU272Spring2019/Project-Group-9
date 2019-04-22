1c7fc240b0fa625e2ad1948944027fbdca8089fa
KavyaChennoju97 <kavya.chennoju@sjsu.edu>
2019-04-22 13:32:11 -0700

    EP-002 Added ProductDisplay Page

--- ergoperfecto/Frontend/src/Components/Main.js
+++ ergoperfecto/Frontend/src/Components/Main.js
@@ -3,13 +3,17 @@ import {Route} from 'react-router-dom';
 // import SignIn from './Components/SignIn';
 // import SignUp from './Components/SignUp';
 import Header from './Navigation/Header';
+import Login from './Navigation/Login';
+import ProductDisplay from './Navigation/ProductDisplay'
 // import '../App.css';
 
 class Main extends Component {
   render() {
     return (
       <div>
-        <Route path = "/header" component = {Header} />
+        <Route path = "/" component = {Header} />
+        <Route path = "/productdisplay" component = {ProductDisplay} />
+        <Route path = "/login" component = {Login} />
       </div>
     );
   }
--- ergoperfecto/Frontend/src/Components/Navigation/Header.js
+++ ergoperfecto/Frontend/src/Components/Navigation/Header.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import '../../Styles/Navigation.css';
+import '../../Styles/Header.css';
 
 class Header extends Component {
+
   render() {
     return (
+ 
+  
       <div className="row">
           <div className="header">
             ERGOPERFECTO
           </div>
       </div>
+    
     );
   }
 }
